6d67aef8a2733eeaaac3a8b1e04bb75bb01a698a
koala_frog <robertalmhaugen@gmail.com>
2023-10-09 09:40:33 +0200

    "installed express-validator"

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,11 @@
-function logger(req, res, next) {
-  console.log(`${req.method} ${req.url} - ${new Date()}`);
+const { ExpressValidator } = require("express-validator");
+
+ExpressValidator.prototype.validate = function (req, res, next) {
+  const errors = this.validationErrors();
+  if (errors) {
+    return res.status(400).json({ errors });
+  }
   next();
-}
+};
+
+module.exports = ExpressValidator;
